46171025aef5071679563dd1e16ba63d0b06b70a
bzr <aryanbeezadhur@gmail.com>
2024-09-29 00:29:46 +0100

    Optimise

--- atoi/src/main.rs
+++ atoi/src/main.rs
@@ -1,61 +1,179 @@
 // https://leetcode.com/problems/string-to-integer-atoi/
 
-fn main() {}
+fn main() {
+    println!("{}", Solution::my_atoi(" -1010023630o4".to_string()));
+}
 
 struct Solution;
 
 impl Solution {
     pub fn my_atoi(s: String) -> i32 {
-        let mut s = s.trim().chars().collect::<Vec<_>>();
-        let mut output = String::new();
+        let mut s = s.trim().chars().peekable();
 
-        let nums_chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        let mut is_negative = false;
+        let mut output = 0;
 
-        if s.len() == 0 {
-            return 0;
+        if let Some('-') = s.peek() {
+            is_negative = true;
+            s.next();
+        } else if let Some('+') = s.peek() {
+            s.next();
         }
 
-        if s[0] == '-' {
-            output.push('-');
-            let _ = s.remove(0);
-        } else if s[0] == '+' {
-            let _ = s.remove(0);
-        }
-
-        if s.len() == 0 {
-            return 0;
-        }
-
-        while s.len() > 0 && s[0] == '0' {
-            let _ = s.remove(0);
-        }
-
-        if s.len() == 0 {
-            return 0;
-        }
-
-        while s.len() > 0 && nums_chars.contains(&s[0]) {
-            output.push(s[0]);
-            s.remove(0);
-        }
-
-        match output.parse::<i128>() {
-            Ok(x) => {
-                if x > i32::MAX as i128 {
-                    return i32::MAX;
-                } else if x < i32::MIN as i128 {
-                    return i32::MIN;
-                } else {
-                    return x as i32;
+        for n in s {
+            if ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].contains(&n) {
+                if output > i32::MAX / 10 && !is_negative {
+                    output = i32::MAX;
+                    break;
                 }
-            }
-            Err(_) => {
-                if s.iter().any(|&c| !nums_chars.contains(&c)) {
-                    return 0;
-                } else {
-                    return i32::MAX;
+
+                if output * -1 < i32::MIN / 10 && is_negative {
+                    output = i32::MIN;
+                    is_negative = false;
+                    break;
                 }
+
+                output *= 10;
+
+                match n {
+                    '0' => (),
+                    '1' => {
+                        if output > i32::MAX - 1 && !is_negative {
+                            output = i32::MAX;
+                            break;
+                        }
+
+                        if output * -1 < i32::MIN + 1 && is_negative {
+                            output = i32::MIN;
+                            is_negative = false;
+                            break;
+                        }
+
+                        output += 1;
+                    }
+                    '2' => {
+                        if output > i32::MAX - 2 && !is_negative {
+                            output = i32::MAX;
+                            break;
+                        }
+
+                        if output * -1 < i32::MIN + 2 && is_negative {
+                            output = i32::MIN;
+                            is_negative = false;
+                            break;
+                        }
+
+                        output += 2;
+                    }
+                    '3' => {
+                        if output > i32::MAX - 3 && !is_negative {
+                            output = i32::MAX;
+                            break;
+                        }
+
+                        if output * -1 < i32::MIN + 3 && is_negative {
+                            output = i32::MIN;
+                            is_negative = false;
+                            break;
+                        }
+
+                        output += 3;
+                    }
+                    '4' => {
+                        if output > i32::MAX - 4 && !is_negative {
+                            output = i32::MAX;
+                            break;
+                        }
+
+                        if output * -1 < i32::MIN + 4 && is_negative {
+                            output = i32::MIN;
+                            is_negative = false;
+                            break;
+                        }
+
+                        output += 4;
+                    }
+                    '5' => {
+                        if output > i32::MAX - 5 && !is_negative {
+                            output = i32::MAX;
+                            break;
+                        }
+
+                        if output * -1 < i32::MIN + 5 && is_negative {
+                            output = i32::MIN;
+                            is_negative = false;
+                            break;
+                        }
+
+                        output += 5;
+                    }
+                    '6' => {
+                        if output > i32::MAX - 6 && !is_negative {
+                            output = i32::MAX;
+                            break;
+                        }
+
+                        if output * -1 < i32::MIN + 6 && is_negative {
+                            output = i32::MIN;
+                            is_negative = false;
+                            break;
+                        }
+
+                        output += 6;
+                    }
+                    '7' => {
+                        if output > i32::MAX - 7 && !is_negative {
+                            output = i32::MAX;
+                            break;
+                        }
+
+                        if output * -1 < i32::MIN + 7 && is_negative {
+                            output = i32::MIN;
+                            is_negative = false;
+                            break;
+                        }
+
+                        output += 7;
+                    }
+                    '8' => {
+                        if output > i32::MAX - 8 && !is_negative {
+                            output = i32::MAX;
+                            break;
+                        }
+
+                        if output * -1 < i32::MIN + 8 && is_negative {
+                            output = i32::MIN;
+                            is_negative = false;
+                            break;
+                        }
+
+                        output += 8;
+                    }
+                    '9' => {
+                        if output > i32::MAX - 9 && !is_negative {
+                            output = i32::MAX;
+                            break;
+                        }
+
+                        if output * -1 < i32::MIN + 9 && is_negative {
+                            output = i32::MIN;
+                            is_negative = false;
+                            break;
+                        }
+
+                        output += 9;
+                    }
+                    _ => (),
+                }
+            } else {
+                break;
             }
+        }
+
+        if is_negative {
+            output * -1
+        } else {
+            output
         }
     }
 }
