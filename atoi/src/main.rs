769c1bdf33c261f4120e501348fe158f6a2f270d
cobrexus <aryanbeezadhur@gmail.com>
2024-12-09 07:11:09 +0000

    Optimise atoi

--- atoi/src/main.rs
+++ atoi/src/main.rs
@@ -6,169 +6,138 @@ struct Solution;
 
 impl Solution {
     pub fn my_atoi(s: String) -> i32 {
-        let mut s = s.trim().chars().peekable();
+        let s = s.as_bytes();
+        let mut s_start: usize = 0;
+        let s_end = s.len();
 
-        let mut is_negative = false;
         let mut output = 0;
+        let mut negative = false;
 
-        if let Some('-') = s.peek() {
-            is_negative = true;
-            s.next();
-        } else if let Some('+') = s.peek() {
-            s.next();
+        if s_start == s_end {
+            return output;
         }
 
-        for n in s {
-            if ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].contains(&n) {
-                if output > i32::MAX / 10 && !is_negative {
-                    output = i32::MAX;
-                    break;
+        while s_start != s_end && s[s_start] == b' ' {
+            s_start += 1;
+        }
+
+        if s_start == s_end {
+            return output;
+        }
+
+        if s[s_start] == b'-' {
+            negative = true;
+            s_start += 1;
+        } else if s[s_start] == b'+' {
+            s_start += 1;
+        }
+
+        if s_start == s_end {
+            return output;
+        }
+
+        while s_start != s_end && s[s_start] == b'0' {
+            s_start += 1;
+        }
+
+        while s_start != s_end {
+            if s[s_start] == b'0' {
+                if negative && i32::MIN / 10 > output * -1 {
+                    return i32::MIN;
+                } else if !negative && i32::MAX / 10 < output {
+                    return i32::MAX;
+                } else {
+                    output *= 10;
                 }
-
-                if output * -1 < i32::MIN / 10 && is_negative {
-                    output = i32::MIN;
-                    is_negative = false;
-                    break;
+            } else if s[s_start] == b'1' {
+                if negative && (i32::MIN + 1) / 10 > output * -1 {
+                    return i32::MIN;
+                } else if !negative && (i32::MAX - 1) / 10 < output {
+                    return i32::MAX;
+                } else {
+                    output *= 10;
+                    output += 1;
                 }
-
-                output *= 10;
-
-                match n {
-                    '0' => (),
-                    '1' => {
-                        if output > i32::MAX - 1 && !is_negative {
-                            output = i32::MAX;
-                            break;
-                        }
-
-                        if output * -1 < i32::MIN + 1 && is_negative {
-                            output = i32::MIN;
-                            is_negative = false;
-                            break;
-                        }
-
-                        output += 1;
-                    }
-                    '2' => {
-                        if output > i32::MAX - 2 && !is_negative {
-                            output = i32::MAX;
-                            break;
-                        }
-
-                        if output * -1 < i32::MIN + 2 && is_negative {
-                            output = i32::MIN;
-                            is_negative = false;
-                            break;
-                        }
-
-                        output += 2;
-                    }
-                    '3' => {
-                        if output > i32::MAX - 3 && !is_negative {
-                            output = i32::MAX;
-                            break;
-                        }
-
-                        if output * -1 < i32::MIN + 3 && is_negative {
-                            output = i32::MIN;
-                            is_negative = false;
-                            break;
-                        }
-
-                        output += 3;
-                    }
-                    '4' => {
-                        if output > i32::MAX - 4 && !is_negative {
-                            output = i32::MAX;
-                            break;
-                        }
-
-                        if output * -1 < i32::MIN + 4 && is_negative {
-                            output = i32::MIN;
-                            is_negative = false;
-                            break;
-                        }
-
-                        output += 4;
-                    }
-                    '5' => {
-                        if output > i32::MAX - 5 && !is_negative {
-                            output = i32::MAX;
-                            break;
-                        }
-
-                        if output * -1 < i32::MIN + 5 && is_negative {
-                            output = i32::MIN;
-                            is_negative = false;
-                            break;
-                        }
-
-                        output += 5;
-                    }
-                    '6' => {
-                        if output > i32::MAX - 6 && !is_negative {
-                            output = i32::MAX;
-                            break;
-                        }
-
-                        if output * -1 < i32::MIN + 6 && is_negative {
-                            output = i32::MIN;
-                            is_negative = false;
-                            break;
-                        }
-
-                        output += 6;
-                    }
-                    '7' => {
-                        if output > i32::MAX - 7 && !is_negative {
-                            output = i32::MAX;
-                            break;
-                        }
-
-                        if output * -1 < i32::MIN + 7 && is_negative {
-                            output = i32::MIN;
-                            is_negative = false;
-                            break;
-                        }
-
-                        output += 7;
-                    }
-                    '8' => {
-                        if output > i32::MAX - 8 && !is_negative {
-                            output = i32::MAX;
-                            break;
-                        }
-
-                        if output * -1 < i32::MIN + 8 && is_negative {
-                            output = i32::MIN;
-                            is_negative = false;
-                            break;
-                        }
-
-                        output += 8;
-                    }
-                    '9' => {
-                        if output > i32::MAX - 9 && !is_negative {
-                            output = i32::MAX;
-                            break;
-                        }
-
-                        if output * -1 < i32::MIN + 9 && is_negative {
-                            output = i32::MIN;
-                            is_negative = false;
-                            break;
-                        }
-
-                        output += 9;
-                    }
-                    _ => (),
+            } else if s[s_start] == b'2' {
+                if negative && (i32::MIN + 2) / 10 > output * -1 {
+                    return i32::MIN;
+                } else if !negative && (i32::MAX - 2) / 10 < output {
+                    return i32::MAX;
+                } else {
+                    output *= 10;
+                    output += 2;
+                }
+            } else if s[s_start] == b'3' {
+                if negative && (i32::MIN + 3) / 10 > output * -1 {
+                    return i32::MIN;
+                } else if !negative && (i32::MAX - 3) / 10 < output {
+                    return i32::MAX;
+                } else {
+                    output *= 10;
+                    output += 3;
+                }
+            } else if s[s_start] == b'4' {
+                if negative && (i32::MIN + 4) / 10 > output * -1 {
+                    return i32::MIN;
+                } else if !negative && (i32::MAX - 4) / 10 < output {
+                    return i32::MAX;
+                } else {
+                    output *= 10;
+                    output += 4;
+                }
+            } else if s[s_start] == b'5' {
+                if negative && (i32::MIN + 5) / 10 > output * -1 {
+                    return i32::MIN;
+                } else if !negative && (i32::MAX - 5) / 10 < output {
+                    return i32::MAX;
+                } else {
+                    output *= 10;
+                    output += 5;
+                }
+            } else if s[s_start] == b'6' {
+                if negative && (i32::MIN + 6) / 10 > output * -1 {
+                    return i32::MIN;
+                } else if !negative && (i32::MAX - 6) / 10 < output {
+                    return i32::MAX;
+                } else {
+                    output *= 10;
+                    output += 6;
+                }
+            } else if s[s_start] == b'7' {
+                if negative && (i32::MIN + 7) / 10 > output * -1 {
+                    return i32::MIN;
+                } else if !negative && (i32::MAX - 7) / 10 < output {
+                    return i32::MAX;
+                } else {
+                    output *= 10;
+                    output += 7;
+                }
+            } else if s[s_start] == b'8' {
+                if negative && (i32::MIN + 8) / 10 > output * -1 {
+                    return i32::MIN;
+                } else if !negative && (i32::MAX - 8) / 10 < output {
+                    return i32::MAX;
+                } else {
+                    output *= 10;
+                    output += 8;
+                }
+            } else if s[s_start] == b'9' {
+                if negative && (i32::MIN + 9) / 10 > output * -1 {
+                    return i32::MIN;
+                } else if !negative && (i32::MAX - 9) / 10 < output {
+                    return i32::MAX;
+                } else {
+                    output *= 10;
+                    output += 9;
                 }
             } else {
                 break;
             }
+
+            s_start += 1;
         }
 
-        if is_negative {
+        if negative {
             output * -1
         } else {
             output
