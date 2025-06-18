11b5fc5bd8d2779500bb4b5c3fa5a3bbf0a7c232
cobrexus <aryanbeezadhur@gmail.com>
2025-06-18 19:38:15 +0100

    Clean up

--- cpp/add-two-numbers.cpp
+++ cpp/add-two-numbers.cpp
@@ -13,33 +13,33 @@
 class Solution {
   private:
     pair<int, int> add(int n1, int n2, int carry) {
-        int sum{n1 + n2 + carry};
-        int new_val{sum % 10};
-        int new_carry{sum / 10};
+        int sum = n1 + n2 + carry;
+        int new_val = sum % 10;
+        int new_carry = sum / 10;
         return {new_val, new_carry};
     }
 
     void add_node(int val, ListNode*& tail) {
-        ListNode* temp{new ListNode{val}};
+        ListNode* temp = new ListNode{val};
         tail->next = temp;
         tail = tail->next;
     }
 
   public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* new_list{new ListNode{}};
-        ListNode* tail{new_list};
+        ListNode* new_list = new ListNode{};
+        ListNode* tail = new_list;
 
-        int carry{};
-        int val1{};
-        int val2{};
-        int current_digit{};
+        int carry = 0;
+        int val1 = 0;
+        int val2 = 0;
+        int current_digit = 0;
 
         while (l1 || l2 || carry) {
             val1 = l1 ? l1->val : 0;
             val2 = l2 ? l2->val : 0;
 
-            pair<int, int> p{add(val1, val2, carry)};
+            pair<int, int> p = add(val1, val2, carry);
             current_digit = p.first;
             carry = p.second;
 
