2ac129e3517db100358b185a624dac8292f67c9f
cobrexus <aryanbeezadhur@gmail.com>
2025-06-19 17:32:57 +0100

    refactor

--- rust/add-two-numbers/src/main.rs
+++ rust/add-two-numbers/src/main.rs
@@ -5,34 +5,20 @@ impl Solution {
         mut l1: Option<Box<ListNode>>,
         mut l2: Option<Box<ListNode>>,
     ) -> Option<Box<ListNode>> {
-        fn add(n1: i32, n2: i32, carry: i32) -> (i32, i32) {
-            let sum = n1 + n2 + carry;
-            let new_val = sum % 10;
-            let new_carry = sum / 10;
-
-            (new_val, new_carry)
-        }
-
         let mut head = Box::new(ListNode { val: 0, next: None });
         let mut tail = &mut head.next;
 
         let mut carry = 0;
-        let mut val1;
-        let mut val2;
-        let mut new_val;
 
-        loop {
-            val1 = match l1 {
-                Some(ref node) => node.val,
+        while (&l1, &l2, carry) != (&None, &None, 0) {
+            let [val1, val2] = [&l1, &l2].map(|l| match l {
+                Some(node) => node.val,
                 None => 0,
-            };
+            });
 
-            val2 = match l2 {
-                Some(ref node) => node.val,
-                None => 0,
-            };
-
-            (new_val, carry) = add(val1, val2, carry);
+            let sum = val1 + val2 + carry;
+            let new_val = sum % 10;
+            carry = sum / 10;
 
             tail = &mut tail
                 .insert(Box::new(ListNode {
@@ -41,16 +27,10 @@ impl Solution {
                 }))
                 .next;
 
-            if let Some(node) = l1 {
-                l1 = node.next;
-            }
-
-            if let Some(node) = l2 {
-                l2 = node.next;
-            }
-
-            if l1.is_none() && l2.is_none() && carry == 0 {
-                break;
+            for l in [&mut l1, &mut l2] {
+                if let Some(node) = l.take() {
+                    *l = node.next;
+                }
             }
         }
 
