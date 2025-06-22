46358322b10001aba15aa6a9b74e2f0825d13ca0
cobrexus <aryanbeezadhur@gmail.com>
2025-06-22 14:15:57 +0100

    Add longest substring in C++

---
+++ cpp/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+class Solution {
+  public:
+    int lengthOfLongestSubstring(string s) {
+        if (s.size() < 1) {
+            return s.size();
+        }
+
+        int start{};
+        int longest{};
+
+        unordered_map<char, size_t> pos{};
+
+        for (int end{}; end < s.size(); ++end) {
+            if (pos.contains(s[end]) && pos[s[end]] >= start) {
+                start = pos[s[end]] + 1;
+            }
+
+            pos[s[end]] = end;
+            longest = max(longest, end - start + 1);
+        }
+
+        return longest;
+    }
+};
