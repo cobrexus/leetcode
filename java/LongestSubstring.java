d5b84a76745fbcb17b148a9272f7ccd6647044f0
cobrexus <aryanbeezadhur@gmail.com>
2025-07-03 22:40:44 +0100

    Add LongestSubstring.java

---
+++ java/LongestSubstring.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() < 1) {
+            return s.length();
+        }
+
+        int start = 0;
+        int longest = 0;
+
+        HashMap<Character, Integer> pos = new HashMap();
+
+        for (int end = 0; end < s.length(); ++end) {
+            char c = s.charAt(end);
+
+            if (pos.containsKey(c) && pos.get(c) >= start) {
+                start = pos.get(c) + 1;
+            }
+
+            pos.put(c, end);
+            longest = Math.max(longest, end - start + 1);
+        }
+
+        return longest;
+    }
+}
